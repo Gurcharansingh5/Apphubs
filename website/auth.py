29e8499c427ff0b35db8fb7df27d3ba05831a832
Gurcharansingh5 <gurcharan.tws@tekkiwebsolutions.com>
2021-07-22 15:24:44 +0530

    google login

---
+++ website/auth.py
@@ -0,0 +1,183 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+GOOGLE_CLIENT_ID = "75718528531-dbbvsoetgeivfmk7ij7hr9n4bcg3a16t.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "VPuy2dSYJJjZ3A0jwUvlV8-Q"
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
+from flask_bootstrap import Bootstrap
+from oauthlib.oauth2 import WebApplicationClient
+import requests,json
+import os
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+client = WebApplicationClient(GOOGLE_CLIENT_ID)
+
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template("sign_up.html", user=current_user)
+
+@auth.route("/loginurl")
+def loginURL():
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=request.base_url + "/callback",
+        scope=["openid", "email", "profile"],
+    )
+    print('request URI')
+    print(request_uri)
+    return redirect(request_uri)
+
+def get_google_provider_cfg():
+    return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+@auth.route("/loginurl/callback")
+def callback():
+    # Get authorization code Google sent back to you
+    code = request.args.get("code")
+
+    # Find out what URL to hit to get tokens that allow you to ask for
+    # things on behalf of a user
+    google_provider_cfg = get_google_provider_cfg()
+    token_endpoint = google_provider_cfg["token_endpoint"]
+
+    # Prepare and send request to get tokens! Yay tokens!
+    token_url, headers, body = client.prepare_token_request(
+        token_endpoint,
+        authorization_response=request.url,
+        redirect_url=request.base_url,
+        code=code,
+    )
+    token_response = requests.post(
+        token_url,
+        headers=headers,
+        data=body,
+        auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET),
+    )
+
+    # Parse the tokens!
+    client.parse_request_body_response(json.dumps(token_response.json()))
+
+    # Now that we have tokens (yay) let's find and hit URL
+    # from Google that gives you user's profile information,
+    # including their Google Profile Image and Email
+    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+    uri, headers, body = client.add_token(userinfo_endpoint)
+    userinfo_response = requests.get(uri, headers=headers, data=body)
+
+    # We want to make sure their email is verified.
+    # The user authenticated with Google, authorized our
+    # app, and now we've verified their email through Google!
+    if userinfo_response.json().get("email_verified"):
+        unique_id = userinfo_response.json()["sub"]
+        users_email = userinfo_response.json()["email"]
+        picture = userinfo_response.json()["picture"]
+        users_name = userinfo_response.json()["given_name"]
+    else:
+        return "User email not available or not verified by Google.", 400
+
+    # # # Create a user in our db with the information provided
+    # # # by Google
+    # # user = User(
+    # #     id=15,
+    # #     first_name=users_name, 
+    # #     email=users_email, 
+    # # )
+
+    # # # Doesn't exist? Add to database
+    # # if not User.get(unique_id):
+    # #     db.session.add(user)
+    # #     db.session.commit()
+    # email = 'q@w.com'
+    # first_name = 'quinniw'
+
+    user = User.query.filter_by(email=users_email).first()
+    if not user:
+        print('no user ')
+        password1 = '121212122'
+        new_user = User(id=unique_id,email=users_email, first_name= users_name, password=password1)
+        print(users_email)
+        db.session.add(new_user)
+        db.session.commit()
+
+    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+
+    # # Begin user session by logging the user in
+    login_user(user, remember=True)
+    print('hfasduhsohos')
+    # new_user = User(email='q@w.com', first_name='quinniw', password=generate_password_hash(
+    #             121212122, method='sha256'))
+    # if not User.get(unique_id):
+    #     db.session.add(new_user)
+    #     db.session.commit()
+
+    # Send user back to homepage
+    return redirect(url_for('views.home'))
+
+    # return redirect(url_for("index"))
