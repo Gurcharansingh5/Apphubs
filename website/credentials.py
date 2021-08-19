7c5b086368523a63d5730b1f3c4428067347b912
Gurcharansingh5 <gurcharan.tws@tekkiwebsolutions.com>
2021-08-19 14:37:37 +0530

    credentials from env

--- website/credentials.py
+++ website/credentials.py
@@ -1,13 +1,14 @@
+import os
 # GOOGLE CREDENTIALS
-GOOGLE_CLIENT_ID = "75718528531-dbbvsoetgeivfmk7ij7hr9n4bcg3a16t.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "VPuy2dSYJJjZ3A0jwUvlV8-Q"
+GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
+GOOGLE_CLIENT_SECRET = os.environ['GOOGLE_CLIENT_SECRET']
 GOOGLE_DISCOVERY_URL = (
     "https://accounts.google.com/.well-known/openid-configuration"
 )
 
 # FACEBOOK CREDENTIALS
-FB_CLIENT_ID=2256808184449973    
-FB_CLIENT_SECRET="bc5fa70ff4ff8dd693f804ba4f0db80c"
+FB_CLIENT_ID=os.environ['FB_CLIENT_ID']
+FB_CLIENT_SECRET=os.environ['FB_CLIENT_SECRET']
 FB_AUTHORIZATION_BASE_URL = "https://www.facebook.com/dialog/oauth"
 FB_TOKEN_URL = "https://graph.facebook.com/oauth/access_token"
 FB_SCOPE = ["email","ads_management"]
@@ -16,7 +17,7 @@ URL = "https://9dbce98b6ad8.ngrok.io"
 
 
 # DROPBOX CREDENTIALS
-DROPBOX_CONSUMER_KEY='j3x8xr8e9aqg1gb'
-DROPBOX_CONSUMER_SECRET='kn7mpqs64v0ac8y'
+DROPBOX_CONSUMER_KEY=os.environ['DROPBOX_CONSUMER_KEY']
+DROPBOX_CONSUMER_SECRET=os.environ['DROPBOX_CONSUMER_SECRET']
 
 dropbox=''
