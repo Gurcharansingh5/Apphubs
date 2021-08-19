ce836bb902e7b27b6c27ab30baa4ef4bb8533038
Gurcharansingh5 <gurcharan.tws@tekkiwebsolutions.com>
2021-08-19 15:15:21 +0530

    credentials from env

--- website/credentials.py
+++ website/credentials.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 # GOOGLE CREDENTIALS
 GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID']
 GOOGLE_CLIENT_SECRET = os.environ['GOOGLE_CLIENT_SECRET']
@@ -12,7 +14,7 @@ FB_CLIENT_SECRET=os.environ['FB_CLIENT_SECRET']
 FB_AUTHORIZATION_BASE_URL = "https://www.facebook.com/dialog/oauth"
 FB_TOKEN_URL = "https://graph.facebook.com/oauth/access_token"
 FB_SCOPE = ["email","ads_management"]
-URL = "https://9dbce98b6ad8.ngrok.io"
+URL = "https://e9636a010b2a.ngrok.io"
 
 
 
