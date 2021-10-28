874cc201ed6d95a9475040e5003c5321f3c37cc5
Gurcharansingh5 <gurcharan.tws@tekkiwebsolutions.com>
2021-10-28 11:01:25 +0530

    database to postgres

--- website/__init__.py
+++ website/__init__.py
@@ -9,13 +9,13 @@ from .credentials import DROPBOX_CONSUMER_KEY,DROPBOX_CONSUMER_SECRET
 # from flask_crontab import Crontab
 from .cronjob import main_cron
 db = SQLAlchemy()
-DB_NAME = "database.db"
+# DB_NAME = "database.db"
 # crontab = Crontab()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = 'hjshjhdjahkjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://dgowsdimwesqsc:00a7671e61c13f04520715be7689e7e38b7b87dfee0f5f8a30584fb02093ea6f@ec2-52-86-25-51.compute-1.amazonaws.com:5432/dfldsj5rnos04d'
     db.init_app(app)
     # crontab.init_app(app)
 
